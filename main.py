0ba43e0256febdf1ac3f8eac28db730d2c279435
Protik <protikm67@gmail.com>
2024-12-12 19:39:57 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+st.header(f"DOWNLOAD BALLOON BUSTER!!!:smile:")
+st.text("Please don't make any CHANGES in GAME FILE")
+st.text("Game Devolap By PROTIK M.")
+
+st.image("bbb.png",width=200,caption="GAME LOGO")
+st.text("----------------------")
+st.text("Minimum Requirements")
+st.text("Windows 10,11")
+st.text("2GB RAM")
+st.text("64 Bits")
+st.text("GAME SIZE = 65.19MB")
+st.text("----------------------")
+button=st.link_button("Download",'https://download1500.mediafire.com/6elt038jdxggflcHRnauroUT-p6Udz292105LqsO-O9plM2OgYPwk20-nrPwY40WyNpxCH2RTR7BQIH1h2yYkQNyvmysgU4mDkiwpPVRrpAc-foAe1gSKFv8rfWM6Y6xCSrDK8Cvot10ob4mE8DlNRmxf4fQCtdV8uAsLKtKLY6e/gm8nd254dsggrl1/BALLOON+BUSTER.rar')
+
+
+st.write("")
+if button:
+     
+        st.write('IF Your Game Is Not Downloading,    Please Click Download Button')
+        st.write("WEB APP CODED BY PROTIK M.")
+        st.balloons()
